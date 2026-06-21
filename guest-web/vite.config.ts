9eec240b2d653417d6344f85403447f87b038690
Pamudu <pamudu123456789@gmail.com>
2026-06-21 12:55:00 +0530

    update guest web

--- guest-web/vite.config.ts
+++ guest-web/vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/astrology_forum_app/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://swasthi-life-backend.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
